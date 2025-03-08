7d543824015067d4934dafebd1a1286e329fd615
indexduo <83288588+IndexDuo@users.noreply.github.com>
2025-03-08 05:04:27 +0000

    Mar 8, 2025, 12:04 AM

---
+++ index.js
@@ -0,0 +1,15 @@
+// Your code here
+
+
+var questionsArr = [
+    {
+      question: 'Who created JavaScript?',
+      answer: 'Brendan Eich',
+      options: [
+        'Linus Torvalds',
+        'Brendan Eich',
+        'Dan Abramov',
+        'Douglas Crockford',
+      ]
+    },
+  ]
