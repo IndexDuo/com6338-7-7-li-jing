c0cf7aa777ca4b83ad4463d6109c1c021d7e113a
indexduo <83288588+IndexDuo@users.noreply.github.com>
2025-03-08 06:31:38 +0000

    Mar 8, 2025, 1:31 AM

--- index.js
+++ index.js
@@ -51,18 +51,7 @@ var timerPara = document.createElement("p");
 var timer;
 var correct = 0;
 var questionNumber = 0;
-var countdown = setInterval(function () {
-    timerPara.textContent = timer;
 
-    console.log("timer:", timer);
-    timerPara.textContent = timer;
-
-    if (timer <= 0) {
-        clearInterval(intervalId); // Stop after 5 iterations
-        console.log("Interval stopped.");
-    }
-    timer--;
-}, 1000);
 var questionAnswer = questionsArr[questionNumber].answer;
 startButton.textContent = "Start Quiz!";
 quizDiv.appendChild(startButton);
@@ -72,7 +61,7 @@ startButton.addEventListener("click", showQuestions);
 function showQuestions() {
     console.log("clicked");
     timer = 30;
-    countdown;
+    startTimer();
     quizDiv.innerHTML = "";
     answerDiv.innerHTML = "";
     if (questionNumber < questionsArr.length) {
@@ -105,6 +94,21 @@ function showQuestions() {
     }
 
     quizDiv.append(questionPara, answerDiv, timerPara);
+
+    function startTimer() {
+        setInterval(function () {
+            timerPara.textContent = timer;
+
+            console.log("timer:", timer);
+            timerPara.textContent = timer;
+
+            if (timer <= 0) {
+                clearInterval(intervalId); // Stop after 5 iterations
+                console.log("Interval stopped.");
+            }
+            timer--;
+        }, 1000);
+    }
 
     // questionsArr.forEach((quiz) => {
     //     // console.log(quiz.question);
